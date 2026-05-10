8ada235608725aa4b4e1424bdb5d770c57804256
Lohith K S <lohithsubraya@feserver.mirafra.com>
2026-05-10 13:46:52 +0530

    Driver updated

---
+++ Driver.v
@@ -0,0 +1,42 @@
+
+module Driver #(parameter WIDTH = 8, parameter CMD_WIDTH = 4)(input CLK,  
+    output reg MODE, CE,
+    output reg [1:0] INP_VALID,
+    output reg [CMD_WIDTH-1:0] CMD,
+    output reg [WIDTH-1:0] OPA, OPB,
+    output reg CIN
+              
+);
+ integer i;             
+ task basic_arithmatic_operation;
+    begin
+            for(i=0;i<16;i=i+1) begin
+            @(posedge CLK) begin
+                CMD <= i;           
+                INP_VALID <= 2'b11;
+                OPA <= $urandom_range(0,127);
+                OPB <= $urandom_range(0,127);
+                CIN <= $urandom_range(0,1);  
+                MODE<= 1;
+            end
+            end
+            end
+                
+    endtask
+   task basic_logical_operation; 
+        begin
+            for(i=0;i<16;i=i+1) begin
+            @(posedge CLK) begin
+                CMD <= i;           
+                INP_VALID <= 2'b11;
+                OPA <= $urandom_range(0,127);
+                OPB <= $urandom_range(0,127);
+                CIN <= $urandom_range(0,1);  
+                MODE<= 0;
+            end
+            end
+            end
+                
+    endtask
+endmodule
+
