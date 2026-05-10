407ef1e7b632291972458e9afd362cbd3d53bb69
Lohith K S <lohithsubraya@feserver.mirafra.com>
2026-05-10 14:19:13 +0530

    driver

--- Driver.v
+++ Driver.v
@@ -2,8 +2,8 @@
 module Driver #(parameter WIDTH = 8, parameter CMD_WIDTH = 4)(input CLK,  
     output reg MODE, CE,
     output reg [1:0] INP_VALID,
-    output reg [`CMD_WIDTH-1:0] CMD,
-    output reg [`WIDTH-1:0] OPA, OPB,
+    output reg [CMD_WIDTH-1:0] CMD,
+    output reg [WIDTH-1:0] OPA, OPB,
     output reg CIN
               
 );
